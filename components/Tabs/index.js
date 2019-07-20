7ff8d70c297fe915ce249e455dc3d07989033f7a
brit-starks <brittneytechiethomas@gmail.com>
2019-07-19 19:50:32 -0700

    Start tabs component

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,34 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+
+axios.get(' https://lambda-times-backend.herokuapp.com/topics')
+  .then(data => {
+    const topic = data;
+    console.log(topic);
+  })
+  .catch(err => 
+    console.log( err, 'You messed up...')
+  );
+
+  const tabs = () => {
+    const topic = document.querySelector('.topics');
+
+    const newTabs = document.createElement('div');
+
+    const tabTopic = document.createElement('div');
+    newTabs.appendChild(tabTopic);
+
+    topic.appendChild(newTabs);
+  }
+  
+  // console.log(tabs);
+
+  topic.map((topic) => {
+    const newTabTopic = new tabs(
+      tabs(),
+      topic.topics
+    );
+    console.log(newTabTopic);
+  })
